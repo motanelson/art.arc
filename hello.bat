ca5fb6d15df90d4903b0139fdd7d4a9d65aa4037
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-05 01:06:11 +0100

    Add files via upload

---
+++ hello.bat
@@ -0,0 +1,27 @@
+class Main(){
+     adds(a,b){
+         return a - b;
+     
+
+     }
+
+
+     subs(a,b){
+          return a + b;
+     
+
+     }
+
+    bcc(a){
+        b=0;
+        if a==0 {
+            b=1;
+        }
+    }
+
+    main(){
+        echo "hello world...\n";
+        return 0;
+    }
+
+}
